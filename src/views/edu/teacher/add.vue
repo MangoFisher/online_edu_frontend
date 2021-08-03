8f106faa210518e4632b8bdb101ced979c29412c
mangofish <576027405@qq.com>
2021-08-03 13:37:35 +0800

    使用第三方插件ImageCropper和PanThumb完成头像上传功能

--- src/views/edu/teacher/add.vue
+++ src/views/edu/teacher/add.vue
@@ -23,7 +23,31 @@
             <el-form-item label="讲师简介"> 
                 <el-input v-model="teacher.intro" :rows="10" type="textarea"/>
             </el-form-item>
-            <!-- 讲师头像：TODO -->
+
+            <!-- 讲师头像 -->
+            <el-form-item label="讲师头像">
+                <!-- 头衔缩略图 -->
+                <pan-thumb :image="teacher.avatar"/>
+                <!-- 文件上传按钮 -->
+                <el-button type="primary" icon="el-icon-upload" @click="imagecropperShow=true">更换头像</el-button>
+                <!--
+                v-show：是否显示上传组件
+                :key：类似于id，如果一个页面多个图片上传控件，可以做区分
+                :url：后台上传的url地址
+                @close：关闭上传组件
+                @crop-upload-success：上传成功后的回调 -->
+                <image-cropper
+                    v-show="imagecropperShow"
+                    :width="300"
+                    :height="300"
+                    :key="imagecropperKey"
+                    :url="'BASE_API' + '/serviceoss/file/upload'"
+                    field="file"
+                    @close="close"
+                    @crop-upload-success="cropSuccess"/>
+            </el-form-item>
+
+
             <el-form-item> 
                 <el-button type="primary" @click="updateOrSave">保存</el-button>
             </el-form-item>
@@ -34,7 +58,10 @@
 
 <script>
 import teacher from '@/api/teacher/teacher'
+import ImageCropper from '@/components/ImageCropper'
+import PanThumb from '@/components/PanThumb'
 export default {
+    components: {ImageCropper, PanThumb}, //引入后还需要声明，否则仍然无法使用
     data() {
         return{
             teacher: {
@@ -44,7 +71,13 @@ export default {
                 career: '',
                 intro: '',
                 avatar: ''
-            }
+            },
+            //文件上传弹窗组件是否显示
+            imagecropperShow: false,
+            imagecropperKey: 0,
+            //从配置文件中取值
+            BASE_API: process.env.BASE_API,
+           
         }
     },
     methods: {
@@ -66,7 +99,18 @@ export default {
             }).catch(error => {
                         console.log(error)
                     })
+        },
+
+        //关闭上传弹窗
+        close() {
+            this.imagecropperShow = false
+        },
+        //上传成功后的回调函数
+        cropSuccess(data) {
+            this.imagecropperShow = false
+            this.avatar = data.url //相当于response.data.url
         }
+
     },
     created() {
         //新增和修改使用同一个组件add.vue,所以需要通过判断路由中是否有id字段来区分
