7fed0299257cc63dbb965d2e45d3d0337d2f046f
mangofish <576027405@qq.com>
2021-08-08 22:32:26 +0800

    1.完成课程分类列表展示功能

--- src/views/edu/subject/list.vue
+++ src/views/edu/subject/list.vue
@@ -1,9 +1,56 @@
 <template>
-    <div>
-        subject list page
-    </div>
+  <div class="app-container">
+    <el-input v-model="filterText" placeholder="Filter keyword" style="margin-bottom:30px;" />
+
+    <el-tree
+      ref="tree2"
+      :data="data2"
+      :props="treeProps"
+      :filter-node-method="filterNode"
+      class="filter-tree"
+      :default-expand-all="true"
+    />
+
+  </div>
 </template>
 
-<style lang="less" scoped>
+<script>
+import subject from '@/api/subject/subject'
+export default {
 
-</style>
+  data() {
+    return {
+      filterText: '',
+      data2: [],
+      treeProps: {
+        children: 'children',
+        label: 'title'
+      }
+    }
+  },
+  watch: {
+    filterText(val) {
+      this.$refs.tree2.filter(val)
+    }
+  },
+
+  methods: {
+    filterNode(value, data) {
+      if (!value) return true
+      return data.label.indexOf(value) !== -1
+    },
+    //获取一级、二级分类数据
+    getAllSubject(){
+        subject.getAllSubject().then(response => {
+            this.data2 = response.data.list
+        }).catch(error => {
+            console.log(error)
+        })
+    }
+  },
+  created() {
+      this.getAllSubject()
+  }
+}
+</script>
+
